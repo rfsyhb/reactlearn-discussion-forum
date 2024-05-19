8ba1a7a0f1fc69fae5d00178279ff28242543afe
rfsyhb <rafisyihab3@gmail.com>
2024-05-20 01:05:28 +0700

    end-to-end test login dengan cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,103 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/**
+ * - Login spec
+ *   - should display login page correctly
+ *   - should display alert when email is empty
+ *   - should display alert when email is not valid
+ *   - should display alert when password is empty
+ *   - should display alert when email and password are wrong
+ *   - should display homepage when email and password are correct
+ */
+describe('Login spec', () => {
+  // memastikan tidak ada duplikasi kode cy.visit maka gunakan beforeEach
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+
+  it('should display login page correctly', () => {
+    // memverifikasi elemen yang harus tampak pada halaman login
+    cy.get('input[placeholder="Email"]').should('be.visible');
+    cy.get('input[placeholder="Password"]').should('be.visible');
+    cy.get('button')
+      .contains(/^Login$/) // memastikan nama sesuai string
+      .should('be.visible');
+  });
+
+  it('should display alert when email is empty', () => {
+    // klik tombol login tanpa mengisi email
+    cy.get('button')
+      .contains(/^Login$/)
+      .click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('"email" is not allowed to be empty');
+    });
+  });
+
+  it('should display alert when email is not valid', () => {
+    // mengisi email
+    cy.get('input[placeholder="Email"]').type('testemail');
+
+    // klik tombol login dengan email yang tidak ssah
+    cy.get('button')
+      .contains(/^Login$/)
+      .click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('"email" must be a valid email');
+    });
+  });
+
+  it('should display alert when password is empty', () => {
+    // mengisi email
+    cy.get('input[placeholder="Email"]').type('testemail@example.com');
+
+    // klik tombol login tanpa mengisi password
+    cy.get('button')
+      .contains(/^Login$/)
+      .click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('"password" is not allowed to be empty');
+    });
+  });
+
+  it('should display alert when email and password are wrong', () => {
+    // mengisi email
+    cy.get('input[placeholder="Email"]').type('testuser@gmail.com');
+
+    // mengisi password yang salah
+    cy.get('input[placeholder="Password"]').type('wrong_password');
+
+    // menekan tombol Login
+    cy.get('button')
+      .contains(/^Login$/)
+      .click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('email or password is wrong');
+    });
+  });
+
+  it('should display homepage when email and password are correct', () => {
+    // mengisi email
+    cy.get('input[placeholder="Email"]').type('capibara@gmail.com');
+
+    // mengisi password
+    cy.get('input[placeholder="Password"]').type('capibara');
+
+    // menekan tombol Login
+    cy.get('button')
+      .contains(/^Login$/)
+      .click();
+
+    // memverifikasi bahwa elemen yang berada di homepage ditampilkan
+    cy.get('button')
+      .contains('Show All')
+      .should('be.visible');
+    cy.get('button').contains('Logout').should('be.visible');
+  });
+});
