1fed8647b3af9b95c3d714ce89c934760f718004
rfsyhb <rafisyihab3@gmail.com>
2024-05-20 00:29:06 +0700

    config cypress

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, 'cypress/globals': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -11,14 +11,14 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'cypress'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    "no-underscore-dangle": "off",
-    "no-alert": "off",
-    "react/jsx-props-no-spreading": "off",
-    "import/prefer-default-export": "off",
-    "react/forbid-prop-types": "off",
+    'no-underscore-dangle': 'off',
+    'no-alert': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'react/forbid-prop-types': 'off',
     'react-refresh/only-export-components': [
       'off',
       { allowConstantExport: true },
